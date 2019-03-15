4545f0c7b978972d7ad62c23cd7a6421b9aea313
VR-DSA <vikram@caltech.edu>
2019-03-15 07:08:05 -0700

    re-adding heimdallr

---
+++ dsaX_dsa5/heimdallr/Scripts/trans_paths.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+#
+# Heimdall python module for path inforamtion
+#
+
+def getControlDir():
+  return "/usr/local/heimdallr/control"
+
+def getConfigDir():
+  return "/usr/local/heimdallr/share"
+
+def getBinaryDir():
+  return "/usr/local/heimdallr/bin"
+
